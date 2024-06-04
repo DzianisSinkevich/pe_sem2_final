d3bdd4d4b52a3bf0b9af5826bc3da905c0575ce9
Denis S <denis13th@gmail.com>
2024-06-04 10:50:10 +0300

    Create logger in main.my. Project restructing.

--- src/main.py
+++ src/main.py
@@ -7,16 +7,27 @@ from data_creation import dc_main
 # from model_preparation import mp_main
 # from model_testing import mt_main
 
+import logging
+
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('logs/log.log')
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 
 # Сообщение-заглушка для метода GET с инструкцией для метода POST
 @app.get("/")
 def get_root():
+    logger.info("START")
     dc_main()
     # dp_main()
     # mp_main()
     # mt_main()
+    logger.info("FINISH\n")
+    logger.info("START results writing")
     with open('results/results', 'r') as f:
         data = f.read()
     print(data)
@@ -28,9 +39,9 @@ def get_root():
     """ + data + """
     </p></body>
     """
+    logger.info("FINISH results writing")
     return HTMLResponse(content=responce_content, status_code=200)
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
-    
