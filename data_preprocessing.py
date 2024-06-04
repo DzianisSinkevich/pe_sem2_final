f2682885a49be92a59acca9f2de7c2c600fb3398
superyulya2008 <148714565+superyulya2008@users.noreply.github.com>
2024-06-04 19:53:37 +0300

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import LabelEncoder  # Импортируем LabelEncoder от scikit-learn
 import pandas as pd  # Библиотека Pandas для работы с табличными данными
 import warnings
+import logging
 
 warnings.filterwarnings('ignore')
 pd.options.mode.chained_assignment = None
@@ -9,22 +10,22 @@ pd.options.mode.chained_assignment = None
 def read_file(file_path):
     try:
         df = pd.read_csv(file_path)
-        print("File " + file_path + " readed successfully.")
+        logger.critical("File " + file_path + " readed successfully.")
         return df
     except IOError:
-        print("Error uccured while readed file '" + file_path + "'.")
+        logger.critical("Error uccured while readed file '" + file_path + "'.")
 
 
 def save_file(df, file_path):
     try:
         df.to_csv(file_path, index=False)
-        print("File " + file_path + " created successfully.")
+        logger.critical("File " + file_path + " created successfully.")
     except IOError:
-        print("Error uccured while creating file " + file_path + " .")
+        logger.critical("Error uccured while creating file " + file_path + " .")
 
 
 def df_prerpocessing(file_path):
-    print("<< Start preprocessing '" + file_path + "' >>")
+    logger.info("<< Start preprocessing '" + file_path + "' >>")
     df = read_file(file_path)
 
     df = df[['shape', 'price', 'carat', 'cut', 'color', 'clarity', 'type']]
@@ -44,13 +45,13 @@ def df_prerpocessing(file_path):
 
     df['cut'] = y_data
     save_file(df, file_path)
-    print("<< Finish preprocessing '" + file_path + "' >>\n")
+    logger.info("<< Finish preprocessing '" + file_path + "' >>\n")
 
 
 def dp_main():
-    print("<<< Start data preprocessing >>>")
+    logger.info("<<< Start data preprocessing >>>")
     train_path = "train/df_train_0.csv"
     df_prerpocessing(train_path)
     test_path = "test/df_test_0.csv"
     df_prerpocessing(test_path)
-    print("<<< Finish data preprocessing >>>\n")
+    logger.info("<<< Finish data preprocessing >>>\n")
