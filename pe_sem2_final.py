18c74fd1a78cb5061de7468f1a2576453be090bb
Bonemarroww <vitaly.kugaevskiy@gmail.com>
2024-06-06 00:46:28 +0500

    Create pe_sem2_final.py

---
+++ pe_sem2_final.py
@@ -0,0 +1,22 @@
+import requests
+import configparser
+
+
+def get():
+    request = requests.get('/')
+    if request.status_code != 200:
+        print("Используй метод POST + данные датасета, чтобы получить результат")
+
+
+def post():
+    new_dct = []
+    resp = requests.post('/', data=new_dct)
+    config = configparser.ConfigParser()
+    config.read('config/settings.ini')
+    config = resp
+    with open('config/settings.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+get()
+post()
