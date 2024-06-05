e06c86b148d6a7ecce8d4a31d5b3c53d6620b117
Bonemarroww <vitaly.kugaevskiy@gmail.com>
2024-06-06 01:35:40 +0500

    Update pe_sem2_final.py

--- pe_sem2_final.py
+++ pe_sem2_final.py
@@ -1,14 +1,1 @@
-import requests
-
-def get():
-    request = requests.get('/')
-    if request.status_code != 200:
-        print("Используй метод POST + данные датасета, чтобы получить результат")
-
-
-def post():
-    new_dct = []
-    resp = requests.post('/', data=new_dct)
-    file = open('config/settings.ini')
-    file.write(resp.text)
 
