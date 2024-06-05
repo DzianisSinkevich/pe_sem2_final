5eede8d936cf0b832b558def83b087572f380a7e
Bonemarroww <vitaly.kugaevskiy@gmail.com>
2024-06-06 01:50:44 +0500

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,2 +1,14 @@
-def test_dummy():
-    pass
+import unittest
+import requests
+class TestAPI(unittest.TestCase):
+  
+    def test_get_request(self):
+
+
+      url = "/"
+      response = requests.get(url)
+      self.assertEqual(response.status_code, 200)
+      self.assertEqual(response.json()["Message"], "Используй метод POST + данные датасета, чтобы получить результат")
+      response = requests.post(url, params=params)
+      self.assertIn("Модель обучена", response.text)
+      
