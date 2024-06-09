956b063451534a43ac8e2cec1677b4d9329a91c3
superyulya2008 <148714565+superyulya2008@users.noreply.github.com>
2024-06-09 13:33:10 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,11 +6,11 @@ class TestAPI(unittest.TestCase):
 
 
       url = "/"
-      #params = {""}
+      params = {"": ""}
       response = requests.get(url)
       self.assertEqual(response.status_code, 200)
       self.assertEqual(response.json()["Message"], "Используй метод POST + данные датасета, чтобы получить результат")
-      response = requests.post(url, params = {""})
+      response = requests.post(url, params = params)
       self.assertIn("Модель обучена", response.text)
       
 
